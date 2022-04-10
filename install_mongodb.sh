6802aea3e1f4381f987edeb5d8143bd2a620a7cc
Свежинцев Георгий <sga@mts.ru>
2022-04-11 00:03:27 +0300

    OTUS HW 6 create VM for run reddit app fix for check

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-ECHO "##############################################"
+sudo apt-get install -y apt-transport-https ca-certificates
+
+echo "##############################################"
 echo "######   add key and repo for mongodb   ######"
-ECHO "##############################################"
+echo "##############################################"
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-ECHO "##############################################"
+echo "##############################################"
 echo "######         INSTALL mongodb          ######"
-ECHO "##############################################"
+echo "##############################################"
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
-ECHO "##############################################"
+echo "##############################################"
 echo "######          start mongodb           ######"
-ECHO "##############################################"
+echo "##############################################"
 
 sudo systemctl start mongod
 sudo systemctl enable mongod
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ECHO "##############################################"
+echo "##############################################"
 echo "INSTALL ruby-full ruby-bundler build-essential"
-ECHO "##############################################"
+echo "##############################################"
 
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
